f27640acbf96adb938cee3420a7a7d416339ad86
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-07-30 19:18:08 +1200

    Update DealOrNoDealGame.java

--- src/main/java/com/mycompany/deal/or/nodeal/DealOrNoDealGame.java
+++ src/main/java/com/mycompany/deal/or/nodeal/DealOrNoDealGame.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.deal.or.nodeal;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rafae
@@ -29,6 +31,66 @@ public class DealOrNoDealGame extends Methods
             }
         }
         System.out.println();
+    }
+    
+    @Override
+    public void selectCase(Cases cases) 
+    {
+        Scanner scan = new Scanner(System.in);
+   
+        System.out.println("Please select a case to keep for the game!");
+        String input = scan.next();
+        int caseNum = Integer.parseInt(input);
+        int playerCase = cases.getCaseNums()[caseNum - 1];
+        cases.getCases().remove(caseNum - 1);
+        cases.getCaseNums()[caseNum - 1] = 0;
+        cases.getCaseValues()[caseNum - 1] = 0.0;
+        System.out.println("You have selected Case " + playerCase + "!");
+        System.out.println();
+        
+    }
+
+    @Override
+    public void playRound(Cases cases, int roundNum) 
+    {
+        int count = 0;
+        int casesToPick = 5;
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Round 1!");
+        while(count < 5)
+        {
+            count++;
+            
+            showCases(cases);
+            System.out.print("Please pick a case! (" + casesToPick +
+                    " more cases to pick.)");
+            String input = scan.next();
+            int caseNum = Integer.parseInt(input);
+            
+            System.out.println("Case " + cases.getCaseNums()[caseNum - 1]
+            + " contains: $" + cases.getCases().get(caseNum - 1));
+            
+            cases.getCases().remove(caseNum - 1);
+            cases.getCaseNums()[caseNum - 1] = 0;
+            cases.getCaseValues()[caseNum - 1] = 0.0;
+            
+            System.out.println("");
+            casesToPick--;
+        }
+        
+    }
+    
+    @Override
+    public void startGame() 
+    {
+        Cases cases = new Cases();
+
+        displayWelcomeMessage();
+        selectCase(cases);
+
+        int roundNum = 1;
+        playRound(cases, roundNum);    
     }
 
 }
