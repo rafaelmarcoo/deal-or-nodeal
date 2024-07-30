d846b1fc8dd58a2c0357d08fab92587d0dc8724b
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-07-30 19:18:29 +1200

    Update Main.java

--- src/main/java/com/mycompany/deal/or/nodeal/Main.java
+++ src/main/java/com/mycompany/deal/or/nodeal/Main.java
@@ -12,10 +12,7 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        Cases cases = new Cases();
         DealOrNoDealGame game = new DealOrNoDealGame();
-        
-        game.displayWelcomeMessage();
-        game.showCases(cases);
+        game.startGame();
     }
 }
