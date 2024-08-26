d718d9f1450ba58d664fc70a42c130f4c78e6ae1
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-08-26 15:41:24 +1200

    Rename files

--- src/main/java/com/mycompany/deal/or/nodeal/IcaseDisplayUI.java
+++ src/main/java/com/mycompany/deal/or/nodeal/IcaseDisplayUI.java
@@ -1,17 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.mycompany.deal.or.nodeal;
 
 /**
  *
- * @author rcman
+ * @author rafae
  */
-
 // Implemented in GameCaseDisplayUI.java
 public interface ICaseDisplayUI 
 {
     public void showCases(Cases cases);
 }
-
--- src/main/java/com/mycompany/deal/or/nodeal/IplayRound.java
+++ src/main/java/com/mycompany/deal/or/nodeal/IplayRound.java
@@ -1,14 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.mycompany.deal.or.nodeal;
 
 /**
  *
- * @author rcman
+ * @author rafae
  */
-
 // Implemented in MechanicsPlayRound.java
 // and MechanicsLastPlay.java
 public interface IPlayRound 
