63b8e5b1734903966c1a864bd7caafba0e65d4b3
rafaelmarcoo <rafaelmarcomanubay@gmail.com>
2024-08-26 15:47:42 +1200

    added files again because i renamed them

---
+++ src/main/java/com/mycompany/deal/or/nodeal/ICaseDisplayUI.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.deal.or.nodeal;
+
+/**
+ *
+ * @author rafae
+ */
+// Implemented in GameCaseDisplayUI.java
+public interface ICaseDisplayUI 
+{
+    public void showCases(Cases cases);
+}
---
+++ src/main/java/com/mycompany/deal/or/nodeal/IPlayRound.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.deal.or.nodeal;
+
+/**
+ *
+ * @author rafae
+ */
+// Implemented in MechanicsPlayRound.java
+// and MechanicsLastPlay.java
+public interface IPlayRound 
+{
+    public void playRound(Cases cases, int roundNum);
+}
